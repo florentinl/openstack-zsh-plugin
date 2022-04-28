a587b79e5fbcd1b7d06b43d5aef4ea54c15251ab
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-04-28 18:57:17 +0200

    Add basic aliases

---
+++ openstack.plugin.zsh
@@ -0,0 +1,33 @@
+source ~/.keystonerc
+
+###############
+### Aliases ###
+###############
+
+# general
+alias o='openstack'
+
+# compute
+alias oc='openstack compute'
+
+# server
+alias os='openstack server'
+alias osl='openstack server list'
+alias ossh='openstack server show'
+alias ossta='openstack server start'
+alias ossto='openstack server stop'
+alias osr='openstack server reboot'
+
+# service
+alias osvc='openstack service'
+alias osvcl='openstack service list'
+
+# port
+alias op='openstack port'
+alias opl='openstack port list'
+alias opsh='openstack port show'
+alias opc='openstack port create'
+alias opd='openstack port delete'
+alias ops='openstack port set'
+alias opu='openstack port unset'
+
