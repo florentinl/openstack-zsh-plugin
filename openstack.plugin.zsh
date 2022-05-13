84b6289af044c8dd7a98c1d15f2bba4f544419fe
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-05-13 18:43:55 +0200

    Add server create aliases

--- openstack.plugin.zsh
+++ openstack.plugin.zsh
@@ -1,22 +1,43 @@
-source ~/.keystonerc
-
 ###############
 ### Aliases ###
 ###############
 
 # general
 alias o='openstack'
+function osw {
+  export OS_CLOUD=$@
+}
 
 # compute
 alias oc='openstack compute'
+alias ocsl='openstack compute service list'
 
 # server
 alias os='openstack server'
 alias osl='openstack server list'
+alias oss='openstack server show'
 alias ossh='openstack server show'
 alias ossta='openstack server start'
 alias ossto='openstack server stop'
 alias osr='openstack server reboot'
+function oscdl {
+  openstack server create \
+      --image "Debian 11 (Bullseye)" \
+      --boot-from-volume 5 \
+      --key-name $1 \
+      --flavor $2 \
+      --network $3 \
+      $4
+}
+function oscndl {
+  openstack server create \
+      --image "Debian 11 (Bullseye)" \
+      --boot-from-volume 5 \
+      --key-name flow \
+      --flavor hosting.nano \
+      --network vm \
+      $@
+}
 
 # service
 alias osvc='openstack service'
@@ -30,4 +51,14 @@ alias opc='openstack port create'
 alias opd='openstack port delete'
 alias ops='openstack port set'
 alias opu='openstack port unset'
+
+# project
+alias opj='openstack project'
+alias opjc='openstack project create'
+alias opjd='openstack project delete'
+alias opjl='openstack project list'
+alias opjs='openstack project set'
+alias opjsh='openstack project show'
+alias opjp='openstack project purge'
+alias opjcl='openstack project cleanup'
 
